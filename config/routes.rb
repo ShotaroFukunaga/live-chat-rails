d20d96bc98e6944e11338b75930fd4405f8be416
ShotaroFukunaga <accelerator.engin@gmail.com>
2022-03-21 12:48:05 +0900

    [add]like_destroy

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
       resources :likes,only:['create']
     end
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :likes, only:['destroy']
+  
 end
